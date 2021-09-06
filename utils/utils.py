2b455d64ea123436221c84bc17dffa0b98c0c4a0
zeqiflow <zeqig@outlook.com>
2021-09-06 11:15:09 +0800

    feat: create constant instructor

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 #    
+from typing import Any
+
 def tf_global_config(intra_threads, inter_threads):
     import tensorflow as tf
     config = tf.ConfigProto(
@@ -9,3 +11,17 @@ def tf_global_config(intra_threads, inter_threads):
     )
     config.gpu_options.allow_growth = True
     tf.Session(config=config).__enter__()
+
+
+class CONST:
+    
+    def __call__(self, name, value):
+        self.__setattr__(name, value)
+        return self.__dict__[name]
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name in self.__dict__:
+            raise self.ConstError("can't rebind const {name}".format(
+                name=name
+            ))
+        self.__dict__[name] = value
