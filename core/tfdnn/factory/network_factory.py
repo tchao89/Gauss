961ec4a944fc712635f5b8a59af8e41039b1f707
zeqiflow <zeqig@outlook.com>
2021-09-29 15:13:58 +0800

    feat: create network factory to distribute different tasks

---
+++ core/tfdnn/factory/network_factory.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021, Citic-Lab. All rights reserved.
+# Authors: citic-lab
+
+from core.tfdnn.networks.mlp_regression_network import MlpRegNetwork
+from core.tfdnn.networks.mlp_classification_network import MlpClsNetwork
+
+class NetworkFactory():
+
+    CLS = "classification"
+    REG = "regression"
+
+    @staticmethod
+    def get_network(task_name):
+        if task_name == NetworkFactory.CLS:
+            return MlpClsNetwork
+        elif task_name == NetworkFactory.REG:
+            return MlpRegNetwork
+        else:
+            raise NotImplementedError(
+                "Current task is not supported yet."
+            )
